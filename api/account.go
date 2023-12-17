91bd36f4eeb5cacbf924fd91d40c95b899c3dc05
Jason Young <jason.manther.young@gmail.com>
2023-12-17 13:11:45 -0600

    Implemented API handlers for accounts.

--- api/account.go
+++ api/account.go
@@ -3,6 +3,7 @@ package api
 import (
 	"database/sql"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 	db "github.com/manther/simplebank/db/sqlc"
@@ -13,6 +14,7 @@ type CreateAccountRequest struct {
 	Currency string `json:"currency" binding:"required,oneof=USD EUR CAD"`
 }
 
+// createAccount - creates a single account from json body
 func (s *Server) createAccount(ctx *gin.Context) {
 	var req CreateAccountRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
@@ -36,13 +38,14 @@ type GetAccountRequest struct {
 	ID int64 `uri:"id" binding:"required,min=1"`
 }
 
-func (s *Server) getAccount(ctx *gin.Context) {
+// getAccount - get single account using uri binding with reqs
+func (server *Server) getAccount(ctx *gin.Context) {
 	var req GetAccountRequest
 	if err := ctx.ShouldBindUri(&req); err != nil {
 		ctx.JSON(http.StatusBadRequest, errorResponse(err))
 		return
 	}
-	account, err := s.store.GetAccount(ctx, req.ID)
+	account, err := server.store.GetAccount(ctx, req.ID)
 	if err != nil {
 		if err == sql.ErrNoRows {
 			ctx.JSON(http.StatusNotFound, errorResponse(err))
@@ -53,4 +56,91 @@ func (s *Server) getAccount(ctx *gin.Context) {
 		return
 	}
 	ctx.JSON(http.StatusOK, account)
+}
+
+type ListAccountsRequest struct {
+	Offset int32 `form:"page_id" binding:"required,min=1"`
+	Limit  int32 `form:"page_size" binding:"required,min=5,max=10"`
+}
+
+// listAccounts - lists accounts using pagination
+func (server *Server) listAccounts(ctx *gin.Context) {
+	var req ListAccountsRequest
+	if err := ctx.ShouldBindQuery(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	accounts, err := server.store.ListAccounts(ctx, db.ListAccountsParams{
+		Limit:  req.Limit,
+		Offset: (req.Offset - 1) * req.Limit,
+	})
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, accounts)
+}
+
+type updateAccountBalance struct {
+	Balance int64 `json:"balance" binding:"required"`
+}
+
+// updateAccount - takes uri id, retrieves it from context params, 
+// and binds update value from json body
+func (server *Server) updateAccount(ctx *gin.Context) {
+	var body updateAccountBalance
+	resID, err := strconv.ParseInt(ctx.Param("id"), 10, 64)
+	if err != nil{
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	if err := ctx.ShouldBindJSON(&body); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+
+	account, err := server.store.UpdateAccount(ctx, db.UpdateAccountParams{
+		ID:      resID,
+		Balance: body.Balance,
+	})
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.JSON(http.StatusOK, account)
+}
+
+type deleteAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+// deleteAccount - delete single account using uri binding with reqs
+func (server *Server) deleteAccount(ctx *gin.Context) {
+	var req deleteAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+		return
+	}
+	err := server.store.DeleteAccount(ctx, req.ID)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
+	}
+	ctx.Status(http.StatusOK)
 }
--- api/server.go
+++ api/server.go
@@ -14,7 +14,9 @@ func NewServer(store *db.Store) *Server {
 	router := gin.Default()
 	router.POST("/accounts", server.createAccount)
 	router.GET("/accounts/:id", server.getAccount)
-	// router.GET("/accounts", server.listAccounts)
+	router.GET("/accounts", server.listAccounts)
+	router.PUT("/accounts/:id", server.updateAccount)
+	router.DELETE("/accounts/:id", server.deleteAccount)
 	// router.POST("/transfers", server.createTransfer)
 	server.router = router
 	return server
